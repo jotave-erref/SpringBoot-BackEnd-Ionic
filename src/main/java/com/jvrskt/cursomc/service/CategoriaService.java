c43651f13a82ea8028c3bb03dcb34bd1ce438d51
Jean Victor <jvrferreira96@gmail.com>
2021-10-22 15:38:48 -0300

    findAll Categoria com DTO

--- src/main/java/com/jvrskt/cursomc/service/CategoriaService.java
+++ src/main/java/com/jvrskt/cursomc/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.jvrskt.cursomc.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +43,10 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e){
 			throw new DataIntegrityException("Não é possivel deletar uma catagoria que possui produtos!");
 		}
-		 
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+		
 	}
 }
