d6fce66631cf623407333f90eecc2791c458085c
Jean Victor <jvrferreira96@gmail.com>
2021-11-11 15:13:29 -0300

    Validação customizada: email não pode ser repetido

--- src/main/java/com/jvrskt/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/jvrskt/cursomc/dto/ClienteNewDTO.java
@@ -2,6 +2,10 @@ package com.jvrskt.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.jvrskt.cursomc.service.validation.ClienteInsert;
 
 @ClienteInsert
@@ -9,8 +13,14 @@ public class ClienteNewDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message="Preenchimento obrigatório!")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório!")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigatório!")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
@@ -18,10 +28,13 @@ public class ClienteNewDTO implements Serializable{
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório!")
 	private String cep;
 	
 	private Integer cidadeId;
 	
+	@NotEmpty(message="Preenchimento obrigatório!")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
--- src/main/java/com/jvrskt/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/jvrskt/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jvrskt.cursomc.domain.Cliente;
 import com.jvrskt.cursomc.domain.enums.TipoCliente;
 import com.jvrskt.cursomc.dto.ClienteNewDTO;
+import com.jvrskt.cursomc.repository.ClienteRepository;
 import com.jvrskt.cursomc.resources.exceptions.FieldMessage;
 import com.jvrskt.cursomc.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Esse email já existe"));
 		}
 		
 		for (FieldMessage e : list) {
